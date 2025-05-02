ec93df349374497ce8724fde2552fff3309ad31c
simonproudfootxyz <simon.d.proudfoot@gmail.com>
2025-05-02 16:18:22 -0400

    Fix styled components

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  output: "export",
+  // output: "export",
+  reactStrictMode: true,
   compiler: {
-    styledComponents: true,
+    styledComponents: true, // Enables styled-components support
   },
 };
 
