23b1938a9f182de983ea64d208fdc8a24c681e9a
simonproudfootxyz <simon.d.proudfoot@gmail.com>
2025-08-01 21:26:21 -0400

    desktop modals in place

--- app/components/Modal/Modal.tsx
+++ app/components/Modal/Modal.tsx
@@ -17,17 +17,78 @@ const Overlay = styled.div`
 
 const ModalBox = styled.div`
   background: var(--cream);
-  padding: 1.5rem 2rem;
-  min-width: 300px;
+  position: relative;
+  padding: 1.5rem 3rem 3rem;
+  width: 100%;
   max-width: 800px;
   color: var(--black);
   clip-path: var(--modal-clip);
+  text-align: center;
 
   p,
   ul,
   li,
-  h2 {
+  h2,
+  h3 {
     color: var(--black);
+  }
+
+  .modal__close {
+    position: absolute;
+    top: 1rem;
+    right: 1rem;
+    border-radius: 50%;
+    background: var(--grey);
+    font-size: 0;
+    padding: 5px 15px;
+
+    .modal__close-symbol {
+      font-size: 1.5rem;
+    }
+  }
+
+  .modal__heading-container {
+    padding: 5rem 3rem 3rem;
+    max-width: 600px;
+    margin: 0 auto 2rem;
+  }
+
+  .modal__heading {
+    max-width: 225px;
+    margin: 0 auto;
+  }
+
+  .modal__image {
+    max-width: 100%;
+    height: auto;
+    animation: hover 5s ease-in-out infinite;
+  }
+
+  .modal__content {
+    max-width: 450px;
+    margin: 0 auto 1rem;
+  }
+
+  // confirm modal
+  &.confirm-modal {
+    .modal__heading-container {
+      background: url("/Vector29.png");
+      background-repeat: no-repeat;
+      background-size: 100% 100%;
+    }
+  }
+
+  // decline modal
+  &.decline-modal {
+    .modal__heading-container {
+      background: url("/Vector30.png");
+      background-repeat: no-repeat;
+      background-size: 100% 100%;
+    }
+
+    .modal__heading {
+      max-width: none;
+    }
   }
 `;
 
@@ -35,19 +96,26 @@ interface ModalProps {
   open: boolean;
   onClose: () => void;
   children: ReactNode;
+  classNames?: string;
 }
 
-export default function Modal({ open, onClose, children }: ModalProps) {
+export default function Modal({
+  open,
+  onClose,
+  children,
+  classNames,
+}: ModalProps) {
   if (!open) return null;
   return (
     <Overlay className="modal__overlay" onClick={onClose}>
       <ModalBox
-        className="modal__box"
+        className={`modal__box ${classNames}`}
         onClick={(e: MouseEvent<HTMLDivElement>) => e.stopPropagation()}
       >
         {children}
         <button className="modal__close" onClick={onClose}>
           Close
+          <span className="modal__close-symbol">&times;</span>
         </button>
       </ModalBox>
     </Overlay>
--- app/rsvp/[id]/page.tsx
+++ app/rsvp/[id]/page.tsx
@@ -7,6 +7,8 @@ import RSVPLayout from "@/app/layouts/rsvp/RSVPLayout";
 import { RSVPForm } from "@/app/components/RSVPForm/RSVPForm";
 import { StylizedButton } from "@/app/components/Button/Button";
 import Modal from "@/app/components/Modal/Modal";
+import Image from "next/image";
+import ButtonLink from "@/app/components/ButtonLink/ButtonLink";
 // import { getAirtableRecords } from "@/app/utilities/airtable";
 
 export default function RSVP() {
@@ -16,33 +18,34 @@ export default function RSVP() {
 
   useEffect(() => {
     // Fetch the guests based on the slug
-    // fetch(`/api/getGuestsByReservation?slug=${slug}`)
-    //   .then((res) => res.json())
-    //   .then((data) => {
-    //     const formattedGuests: Guest[] = data.map((guest) => ({
-    //       id: guest.id,
-    //       fields: guest.fields,
-    //     }));
-    //     setGuests(formattedGuests);
-    //     localStorage.setItem("reservationId", slug);
-    //   })
-    //   .catch((error) =>
-    //     console.error("Error fetching Airtable records:", error)
-    //   );
+    fetch(`/api/getGuestsByReservation?slug=${slug}`)
+      .then((res) => res.json())
+      .then((data) => {
+        const formattedGuests: Guest[] = data.map((guest) => ({
+          id: guest.id,
+          fields: guest.fields,
+        }));
+        setGuests(formattedGuests);
+        localStorage.setItem("reservationId", slug);
+      })
+      .catch((error) =>
+        console.error("Error fetching Airtable records:", error)
+      );
   }, [slug]);
 
-  const [modalOpen, setModalOpen] = useState(false);
+  const [modalConfirmOpen, setModalConfirmOpen] = useState(false);
 
-  const handleConfirm = () => setModalOpen(true);
-  const handleClose = () => setModalOpen(false);
+  const handleConfirm = () => setModalConfirmOpen(true);
+  const handleConfirmClose = () => setModalConfirmOpen(false);
 
-  // if (!guests) {
-  //   return <RSVPLayout loading={true} />;
-  // }
+  const [modalDeclineOpen, setModalDeclineOpen] = useState(false);
 
-  // const handleClick = () => {
-  //   console.log("handleClick");
-  // };
+  const handleDecline = () => setModalDeclineOpen(true);
+  const handleDeclineClose = () => setModalDeclineOpen(false);
+
+  if (!guests) {
+    return <RSVPLayout loading={true} />;
+  }
 
   const hasPluralGuests = guests?.length > 1;
   const spiritText = hasPluralGuests ? "spirits" : "spirit";
@@ -50,18 +53,60 @@ export default function RSVP() {
   const pageTitle = `Welcome, ${spiritText} of ${guestText}!`;
   return (
     <RSVPLayout loading={false} titleText={pageTitle}>
-      {/* <RSVPForm guests={guests} /> */}
+      <RSVPForm guests={guests} />
       <div>
         <StylizedButton type="button" onClick={() => handleConfirm()}>
           Confirm
         </StylizedButton>
-        <StylizedButton type="button" onClick={() => handleConfirm()}>
+        <StylizedButton type="button" onClick={() => handleDecline()}>
           Deny
         </StylizedButton>
       </div>
-      <Modal open={modalOpen} onClose={handleClose}>
-        <h2>Thank you for confirming!</h2>
-        <p>Your RSVP has been received.</p>
+      <Modal
+        classNames="confirm-modal"
+        open={modalConfirmOpen}
+        onClose={handleConfirmClose}
+      >
+        <div className="modal__heading-container">
+          <h2 className="modal__heading">party on, people!</h2>
+          <Image
+            className="modal__image"
+            src="/images/CheersAnimation.png"
+            alt="Cheers"
+            width={407}
+            height={342}
+            priority
+          />
+        </div>
+        <p className="modal__content">
+          Thanks for your RSVP, we look forward to sharing our special day with
+          you this halloween.{" "}
+        </p>
+        <ButtonLink href="/">back to site</ButtonLink>
+      </Modal>
+      <Modal
+        classNames="decline-modal"
+        open={modalDeclineOpen}
+        onClose={handleDeclineClose}
+      >
+        <div className="modal__heading-container">
+          <Image
+            className="modal__image"
+            src="/images/DogAnimation.png"
+            alt="Cheers"
+            width={365}
+            height={295}
+            priority
+          />
+          <h2 className="modal__heading">
+            catch you on the flippity flip, people!
+          </h2>
+        </div>
+        <p className="modal__content">
+          Thanks for letting us know. We’re sorry to miss you but we’ll see you
+          next time!
+        </p>
+        <ButtonLink href="/">back to site</ButtonLink>
       </Modal>
     </RSVPLayout>
   );
