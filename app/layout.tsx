bbafa61b26ba9b0bb18babb7ad3a006cd90437a5
simonproudfootxyz <simon.d.proudfoot@gmail.com>
2026-05-05 15:49:55 -0400

    - fix FOUC

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
-import Head from "next/head";
+import StyledComponentsRegistry from "./lib/registry";
 
 export const metadata: Metadata = {
   title: "Mhairi and Simon Are Getting Married!",
@@ -8,16 +8,14 @@ export const metadata: Metadata = {
     "Mhairi and Simon are getting married, and it's going to kick ass",
 };
 
-// core styles are required for all packages
-
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
-    <>
-      <Head>
+    <html lang="en">
+      <head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link
           rel="preconnect"
@@ -28,20 +26,14 @@ export default function RootLayout({
           href="https://fonts.googleapis.com/css2?family=Old+Standard+TT:ital,wght@0,400;0,700;1,400&display=swap"
           rel="stylesheet"
         />
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link
-          rel="preconnect"
-          href="https://fonts.gstatic.com"
-          crossOrigin="anonymous"
-        />
         <link
           href="https://fonts.googleapis.com/css2?family=Spectral:ital,wght@0,200;0,300;0,400;0,500;0,600;0,700;0,800;1,200;1,300;1,400;1,500;1,600;1,700;1,800&display=swap"
           rel="stylesheet"
-        ></link>
-      </Head>
-      <html lang="en">
-        <body className="page">{children}</body>
-      </html>
-    </>
+        />
+      </head>
+      <body className="page">
+        <StyledComponentsRegistry>{children}</StyledComponentsRegistry>
+      </body>
+    </html>
   );
 }
